faa903f5f4e03b6a4f2401afbd241228d3485775
ViniciusIth <vini.ithalo@hotmail.com>
2024-01-01 11:08:40 -0300

    First Commit

---
+++ src/main/java/cloud/viniciusith/revampedbundles/RevampedBundles.java
@@ -0,0 +1,16 @@
+package cloud.viniciusith.revampedbundles;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RevampedBundles implements ModInitializer {
+    public static final String MOD_ID = "revampedbundle";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        // This method is intentionally left blank
+    }
+}
---
+++ src/main/java/cloud/viniciusith/revampedbundles/client/RevampedBundlesClient.java
@@ -0,0 +1,10 @@
+package cloud.viniciusith.revampedbundles.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RevampedBundlesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
